7e4f954da73fdacb9c54cda0c18f00d5002cc6d1
Alexei <dark_sao@mail.ru>
2024-06-26 15:54:36 +0300

    change cors policy and link front with back

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios';
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const indexExpeditions = async () => {
+    const { data } = await axios.get(`http://127.0.0.1:3001/api/v1/questions`, {
+      params: {}
+    })
+
+    return data
+  }
+
+  indexExpeditions().then((data) => console.log(data))
 
   return (
     <>
