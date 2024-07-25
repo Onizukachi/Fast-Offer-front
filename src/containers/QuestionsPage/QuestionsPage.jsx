4c46792070159a3792ce74eb6ab510fc344cf669
Alexei <dark_sao@mail.ru>
2024-07-25 16:22:05 +0300

    cursor pagination with infinity scroll

--- src/containers/QuestionsPage/QuestionsPage.jsx
+++ src/containers/QuestionsPage/QuestionsPage.jsx
@@ -1,28 +1,52 @@
 import { useQuery } from "react-query";
-import { useState } from 'react'
+import { useState } from "react";
 import { fetchQuestions } from "./queries";
-import { Spinner } from "@nextui-org/react";
 import Question from "@components/Questions/Question";
 import { deserialize } from "deserialize-json-api";
+import InfiniteScroll from "react-infinite-scroll-component";
+import BeatLoader from "react-spinners/BeatLoader";
+
+const LIMIT_PER_PAGE = 10;
 
 const QuestionsPage = () => {
-  const [questionsData, setQuestionsData] = useState([])
-  const { isSuccess, isLoading } = useQuery(
+  const [questionsData, setQuestionsData] = useState([]);
+  const [lastCursor, setLastCursor] = useState(null);
+  const [hasMore, setHasMore] = useState(false);
+
+  const { isLoading, refetch } = useQuery(
     `questions`,
-    () => fetchQuestions().then((data) => {
-      console.log(deserialize(data).data)
-      setQuestionsData(deserialize(data).data)
-    }),
+    () =>
+      fetchQuestions(lastCursor, LIMIT_PER_PAGE).then((data) => {
+        setHasMore(data.meta.has_next);
+        setLastCursor(data.meta.next_cursor);
+        setQuestionsData((prevState) =>
+          prevState.concat(deserialize(data).data),
+        );
+      }),
     { refetchInterval: false, refetchOnWindowFocus: false },
   );
 
   return (
     <div className="flex flex-col gap-4">
-      {isLoading && <Spinner size="lg" color="primary" />}
-      {isSuccess &&
-        questionsData.map((question) => {
+      {isLoading && (
+        <div>
+          <BeatLoader
+            className="mt-8 text-center"
+            size="20px"
+            color="#5c7de0"
+          />
+        </div>
+      )}
+      <InfiniteScroll
+        dataLength={questionsData.length}
+        next={refetch}
+        hasMore={hasMore}
+        loader={<BeatLoader className="mt-8 text-center" size="20px" color="#5c7de0" />}
+      >
+        {questionsData.map((question) => {
           return <Question key={question.id} question={question} />;
         })}
+      </InfiniteScroll>
     </div>
   );
 };
--- src/containers/QuestionsPage/queries.js
+++ src/containers/QuestionsPage/queries.js
@@ -1,10 +1,14 @@
 import api from "@utils/jwtInterceptor.js";
 import { API_BACK_BASE_URL } from "@constants/api.js";
 
-export const fetchQuestions = async () => {
+export const fetchQuestions = async (lastCursor, limit) => {
   const { data } = await api.get(`${API_BACK_BASE_URL}/api/v1/questions`, {
     headers: {
       'Accept': 'application/json'
+    },
+    params: {
+      after: lastCursor,
+      limit: limit
     }
   });
 
