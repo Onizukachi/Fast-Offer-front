ff20dd664a9770747ef8f0fb1c0dfba23fad2f75
Alexei <dark_sao@mail.ru>
2024-07-25 19:52:48 +0300

    fast search with refs

--- src/containers/QuestionsPage/QuestionsPage.jsx
+++ src/containers/QuestionsPage/QuestionsPage.jsx
@@ -1,24 +1,51 @@
 import { useQuery } from "react-query";
-import { useState } from "react";
+import { useState, useCallback, useMemo, useRef } from "react";
 import { fetchQuestions } from "./queries";
 import Question from "@components/Questions/Question";
 import { deserialize } from "deserialize-json-api";
 import InfiniteScroll from "react-infinite-scroll-component";
 import BeatLoader from "react-spinners/BeatLoader";
+import { Input } from "@nextui-org/react";
+import { IoSearchOutline } from "react-icons/io5";
+import { debounce } from "lodash";
 
 const LIMIT_PER_PAGE = 10;
 
 const QuestionsPage = () => {
   const [questionsData, setQuestionsData] = useState([]);
-  const [lastCursor, setLastCursor] = useState(null);
-  const [hasMore, setHasMore] = useState(false);
+  const searchTermRef = useRef("");
+  const hasMoreRef = useRef(false);
+  const cursorRef = useRef(null);
+
+  const resetPageAndData = () => {
+    setQuestionsData([]);
+    cursorRef.current = null;
+  };
+
+  const handleSearch = useCallback(() => {
+    resetPageAndData();
+    refetch();
+  }, []);
+
+  const debouncedSearch = useMemo(() => {
+    return debounce(handleSearch, 500);
+  }, [handleSearch]);
+
+  const handleSearchInputChange = (value) => {
+    searchTermRef.current = value;
+    debouncedSearch();
+  };
 
   const { isLoading, refetch } = useQuery(
     `questions`,
     () =>
-      fetchQuestions(lastCursor, LIMIT_PER_PAGE).then((data) => {
-        setHasMore(data.meta.has_next);
-        setLastCursor(data.meta.next_cursor);
+      fetchQuestions(
+        cursorRef.current,
+        LIMIT_PER_PAGE,
+        searchTermRef.current,
+      ).then((data) => {
+        hasMoreRef.current = data.meta.has_next;
+        cursorRef.current = data.meta.next_cursor;
         setQuestionsData((prevState) =>
           prevState.concat(deserialize(data).data),
         );
@@ -28,6 +55,18 @@ const QuestionsPage = () => {
 
   return (
     <div className="flex flex-col gap-4">
+      <div className="max-w-2xl w-full mx-auto items-center justify-center">
+        <Input
+          type="search"
+          size="lg"
+          onValueChange={handleSearchInputChange}
+          placeholder="Поиск по вопросам"
+          startContent={
+            <IoSearchOutline className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+          }
+        />
+      </div>
+
       {isLoading && (
         <div>
           <BeatLoader
@@ -40,8 +79,14 @@ const QuestionsPage = () => {
       <InfiniteScroll
         dataLength={questionsData.length}
         next={refetch}
-        hasMore={hasMore}
-        loader={<BeatLoader className="mt-8 text-center" size="20px" color="#5c7de0" />}
+        hasMore={hasMoreRef.current}
+        loader={
+          <BeatLoader
+            className="mt-8 text-center"
+            size="20px"
+            color="#5c7de0"
+          />
+        }
       >
         {questionsData.map((question) => {
           return <Question key={question.id} question={question} />;
--- src/containers/QuestionsPage/queries.js
+++ src/containers/QuestionsPage/queries.js
@@ -1,14 +1,15 @@
 import api from "@utils/jwtInterceptor.js";
 import { API_BACK_BASE_URL } from "@constants/api.js";
 
-export const fetchQuestions = async (lastCursor, limit) => {
+export const fetchQuestions = async (lastCursor, limit, query) => {
   const { data } = await api.get(`${API_BACK_BASE_URL}/api/v1/questions`, {
     headers: {
       'Accept': 'application/json'
     },
     params: {
       after: lastCursor,
-      limit: limit
+      limit: limit,
+      query: query
     }
   });
 
