5988450284599e1b2ee3e6f6d9fdb7a86d1b0217
Alexei <dark_sao@mail.ru>
2024-07-31 13:58:59 +0300

    optimization filters and search

--- src/containers/QuestionsPage/QuestionsPage.jsx
+++ src/containers/QuestionsPage/QuestionsPage.jsx
@@ -1,70 +1,147 @@
 import { useQuery } from "react-query";
-import { useState, useCallback, useMemo, useRef } from "react";
+import { useState, useCallback, useRef, useEffect } from "react";
 import { questionsQuery } from "./queries";
 import Question from "@components/Question";
 import { deserialize } from "deserialize-json-api";
 import InfiniteScroll from "react-infinite-scroll-component";
 import BeatLoader from "react-spinners/BeatLoader";
-import {Input, Select, SelectItem, Switch} from "@nextui-org/react";
+import { Input, Select, SelectItem, Switch } from "@nextui-org/react";
 import { IoSearchOutline } from "react-icons/io5";
 import { debounce } from "lodash";
 import { Button } from "@nextui-org/react";
-import { useNavigate } from "react-router-dom";
-import {showToast} from "@utils/toast";
-import {UNPERMITTED} from "@constants/toastMessages";
-import {positionsQuery} from "@queries/positionsQuery";
-import {gradesQuery} from "@queries/gradesQuery";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { showToast } from "@utils/toast";
+import { UNPERMITTED } from "@constants/toastMessages";
+import { positionsQuery } from "@queries/positionsQuery";
+import { gradesQuery } from "@queries/gradesQuery";
 
 const LIMIT_PER_PAGE = 10;
 
-const ORDER_OPTIONS = [{key: 'desc', label: 'По возрастанию'}, {key: 'asc', label: 'По убыванию'}]
+const ORDER_OPTIONS = [
+  { key: "desc", label: "По возрастанию" },
+  { key: "asc", label: "По убыванию" },
+];
 
 const QuestionsPage = () => {
   const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
   const [questionsData, setQuestionsData] = useState([]);
-  const [positions, setPositions] = useState([])
+  const [positions, setPositions] = useState([]);
   const [grades, setGrades] = useState([]);
-  const searchTermRef = useRef("");
+  const [searchTerm, setSearchTerm] = useState(searchParams.get("q") || "");
+  const [selectedGradeId, setSelectedGradeId] = useState(
+    searchParams.get("grade_id"),
+  );
+  const [selectedPositionIds, setSelectedPositionIds] = useState(
+    searchParams.getAll("position_ids") || [],
+  );
   const hasMoreRef = useRef(false);
   const cursorRef = useRef(null);
-  const [showFilters, setShowFilters] = useState(false)
+  const [showFilters, setShowFilters] = useState(false);
 
-  const toggleFilters = () => setShowFilters(!showFilters)
+  useEffect(() => {
+    if (searchParams.get("grade_id")) {
+      setSelectedGradeId(searchParams.get("grade_id"));
+    } else {
+      setSelectedGradeId(null);
+    }
 
-  const cleanRefetch = () => {
+    if (searchParams.get("position_ids")) {
+      setSelectedPositionIds(searchParams.get("position_ids").split(","));
+    } else {
+      setSelectedPositionIds([]);
+    }
+
+    if (searchParams.get("q")) {
+      setSearchTerm(searchParams.get("q"));
+    } else {
+      setSearchTerm("");
+    }
+  }, [searchParams]);
+
+  const toggleFilters = () => setShowFilters(!showFilters);
+
+  const resetMeta = () => {
     setQuestionsData([]);
     cursorRef.current = null;
+  };
+
+  const cleanRefetch = () => {
+    resetMeta();
     refetch();
-  }
+  };
 
-  const handleSearch = useCallback(() => { cleanRefetch() }, []);
+  const handleSearch = useCallback(
+    debounce((searchTerm) => {
+      resetMeta();
+      setSearchParams(
+        (prev) => {
+          prev.set("q", searchTerm);
+          return prev;
+        },
+        { replace: true },
+      );
+    }, 500), // Debounce for 500 milliseconds
+    [searchParams],
+  );
 
-  const debouncedSearch = useMemo(() => {
-    return debounce(handleSearch, 500);
-  }, [handleSearch]);
+  const handleSearchInputChange = (searchTerm) => {
+    handleSearch(searchTerm);
+  };
 
-  const handleSearchInputChange = (value) => {
-    searchTermRef.current = value;
-    debouncedSearch();
+  const handleGradeChange = (e) => {
+    resetMeta();
+    setSearchParams(
+      (prev) => {
+        prev.set("grade_id", e.target.value);
+        return prev;
+      },
+      { replace: true },
+    );
+  };
+
+  const handlePositionsChange = (positionIds) => {
+    resetMeta();
+    setSearchParams(
+      (prev) => {
+        prev.set(
+          "position_ids",
+          Array.from(positionIds)
+            .map((el) => Number(el))
+            .filter((el) => el !== 0)
+            .join(","),
+        );
+        return prev;
+      },
+      { replace: true },
+    );
+  };
+
+  const queryParams = () => {
+    return {
+      after: cursorRef.current,
+      limit: LIMIT_PER_PAGE,
+      query: searchTerm,
+      grade_id: selectedGradeId,
+      position_ids: selectedPositionIds,
+    };
   };
 
   const { isLoading, refetch } = useQuery(
-    `questions`,
+    [searchTerm, selectedGradeId, selectedPositionIds],
     () =>
-      questionsQuery(
-        cursorRef.current,
-        LIMIT_PER_PAGE,
-        searchTermRef.current,
-      ).then((data) => {
-        hasMoreRef.current = data.meta.has_next;
-        cursorRef.current = data.meta.next_cursor;
-        setQuestionsData((prevState) =>
-          prevState.concat(deserialize(data).data),
-        );
-      }).catch((error) => {
-        console.log(error.response.data)
-        if(error.response.status === 401) showToast(UNPERMITTED, 'warning')
-      }),
+      questionsQuery(queryParams())
+        .then((data) => {
+          hasMoreRef.current = data.meta.has_next;
+          cursorRef.current = data.meta.next_cursor;
+          setQuestionsData((prevState) =>
+            prevState.concat(deserialize(data).data),
+          );
+        })
+        .catch((error) => {
+          console.log(error.response.data);
+          if (error.response.status === 401) showToast(UNPERMITTED, "warning");
+        }),
     { refetchInterval: false, refetchOnWindowFocus: false },
   );
 
@@ -89,9 +166,7 @@ const QuestionsPage = () => {
   return (
     <div className="flex flex-col gap-4">
       <div className="flex items-center justify-center flex-wrap gap-6">
-        <Switch onChange={toggleFilters}>
-          Фильтры
-        </Switch>
+        <Switch onChange={toggleFilters}>Фильтры</Switch>
         <div className="max-w-2xl w-full">
           <Input
             type="search"
@@ -99,12 +174,12 @@ const QuestionsPage = () => {
             onValueChange={handleSearchInputChange}
             placeholder="Поиск по вопросам"
             startContent={
-              <IoSearchOutline className="text-2xl text-default-400 pointer-events-none flex-shrink-0"/>
+              <IoSearchOutline className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
             }
           />
         </div>
         <Button
-          onClick={() => navigate("/questions/new", {replace: false})}
+          onClick={() => navigate("/questions/new", { replace: false })}
           color="primary"
           size="lg"
           variant="shadow"
@@ -112,16 +187,16 @@ const QuestionsPage = () => {
           Задать вопрос
         </Button>
       </div>
-      { showFilters && (
+      {showFilters && (
         <div className="flex flex-wrap gap-6 justify-center mt-3">
           <Select
             label="Уровень сложности"
-            // defaultSelectedKeys={[selectedGradeId?.toString()]}
+            defaultSelectedKeys={[selectedGradeId?.toString()]}
             labelPlacement="outside"
             placeholder="Выберите уровень"
             className="max-w-56"
             disableSelectorIconRotation
-            onChange={(e) => console.log(e.target.value)}
+            onChange={handleGradeChange}
           >
             {grades.map((grade) => (
               <SelectItem key={grade.id}>{grade.grade}</SelectItem>
@@ -129,13 +204,13 @@ const QuestionsPage = () => {
           </Select>
           <Select
             label="Языки программирования"
-            // defaultSelectedKeys={selectedPositionIds}
+            defaultSelectedKeys={selectedPositionIds}
             labelPlacement="outside"
             selectionMode="multiple"
             placeholder="Выберите языки"
             className="max-w-56"
             onChange={(e) =>
-              console.log((new Set(e.target.value.split(","))))
+              handlePositionsChange(new Set(e.target.value.split(",")))
             }
           >
             {positions.map((position) => (
@@ -163,7 +238,7 @@ const QuestionsPage = () => {
             onChange={(e) => console.log(e.target.value)}
           >
             {ORDER_OPTIONS.map((option) => (
-              <SelectItem key={option.id}>{option.label}</SelectItem>
+              <SelectItem key={option.key}>{option.label}</SelectItem>
             ))}
           </Select>
           <Select
@@ -176,7 +251,7 @@ const QuestionsPage = () => {
             onChange={(e) => console.log(e.target.value)}
           >
             {ORDER_OPTIONS.map((option) => (
-              <SelectItem key={option.id}>{option.label}</SelectItem>
+              <SelectItem key={option.key}>{option.label}</SelectItem>
             ))}
           </Select>
         </div>
@@ -203,7 +278,13 @@ const QuestionsPage = () => {
         }
       >
         {questionsData.map((question) => {
-          return <Question key={question.id} question={question} refetch={cleanRefetch}/>;
+          return (
+            <Question
+              key={question.id}
+              question={question}
+              refetch={cleanRefetch}
+            />
+          );
         })}
       </InfiniteScroll>
     </div>
--- src/containers/QuestionsPage/queries.js
+++ src/containers/QuestionsPage/queries.js
@@ -1,16 +1,12 @@
 import api from "@utils/jwtInterceptor.js";
 import { API_BACK_BASE_URL } from "@constants/api.js";
 
-export const questionsQuery = async (lastCursor, limit, query) => {
+export const questionsQuery = async (params) => {
   const { data } = await api.get(`${API_BACK_BASE_URL}/api/v1/questions`, {
     headers: {
       'Accept': 'application/json'
     },
-    params: {
-      after: lastCursor,
-      limit: limit,
-      query: query
-    }
+    params: params
   });
 
   return data;
